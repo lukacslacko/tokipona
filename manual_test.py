209d69897665d0bc46088d0a8f8290e5f1e8b18f
lukacslacko <31095308+lukacslacko@users.noreply.github.com>
2021-10-12 20:55:50 +0200

    Send width and height in manual test

--- manual_test.py
+++ manual_test.py
@@ -2,7 +2,7 @@ import render;
 
 render = Render();
 
-render.word("mi", 0, 0, 1/3)
-render.word("moku", 1/3, 0, 1/3)
-render.word("telo", 2/3, 0, 1/3)
+render.word("mi", 0, 0, 1/3, 1/3)
+render.word("moku", 1/3, 0, 1/3, 1/3)
+render.word("telo", 2/3, 0, 1/3, 1/3)
 render.render()
