03a2135a2df50253cea719bc947503046613309e
lukacslacko <31095308+lukacslacko@users.noreply.github.com>
2021-10-12 19:51:44 +0200

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,1 +1,19 @@
+# ((toki | pona) - li[toki | pona]) | .[]
+# (toki - pona) | li[toki - pona] | .[]
+# (mi - nimi) | (jan - cart[La Si]) | .[]
+# (mi - nimi - jan) | cart[La Si] | .[]
+# (Ka - La - (Ma | Pa)) | (lili) | (((Ke - Le) | Me) - (Ki | (Li - Mi)))
+# (nasin mani) | li[ike] | .[]
+#
+# class Render:
+#  def valid_word(self, word: str) -> bool:
+#  def valid_container(self, word: str) -> bool:
+#  def width(self, word: str) -> float:
+#  def height(self, word: str) -> float:
+#  def word(self, word: str, x: float, y: float, size: float) -> None:
+#  def container(self, word: str, x: float, y: float, w: float, h: float) -> None:
+#  def render(self):
+
+import render;
+
 pass
