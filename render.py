1b9123846345cceb07d979449b688d371c5810a4
lukacslacko <31095308+lukacslacko@users.noreply.github.com>
2021-10-12 19:49:38 +0200

    Update render.py

--- render.py
+++ render.py
@@ -1,2 +1,24 @@
 class Render:
-  pass
+  def __init__(self):
+    pass
+  
+  def valid_word(self, word: str) -> bool:
+    return True
+  
+  def valid_container(self, word: str) -> bool:
+    return True
+  
+  def width(self, word: str) -> float:
+    return 1.0
+  
+  def height(self, word: str) -> float:
+    return 1.0
+  
+  def word(self, word: str, x: float, y: float, size: float) -> None:
+    pass
+  
+  def container(self, word: str, x: float, y: float, w: float, h: float) -> None:
+    pass
+  
+  def render(self):
+    pass
