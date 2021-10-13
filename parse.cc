676638f4169fb4c231c5da191f4b5d01df28b4db
lukacslacko <31095308+lukacslacko@users.noreply.github.com>
2021-10-13 20:09:50 +0200

    Update parse.cc

--- parse.cc
+++ parse.cc
@@ -6,6 +6,7 @@
 int main() {
   std::string input = "(((jan|utala)-(pona|ni))|li[lawa]|.[])-((mi-lawa)|e[(jan|utala)-(pona|ni)]|.[])";
   Render render;
+  std::cout << input << std::endl;
   
   parse(input, 7, render);
 }
