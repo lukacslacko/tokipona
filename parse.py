b9221bcbbdebb8a8b6fad5b68c9dbe5cf574347d
QVERTYA <34912538+QVERTYA@users.noreply.github.com>
2021-10-13 09:34:22 +0200

    Create parse.py

--- parse.py
+++ parse.py
@@ -10,10 +10,41 @@
 #  def valid_container(self, word: str) -> bool:
 #  def width(self, word: str) -> float:
 #  def height(self, word: str) -> float:
-#  def word(self, word: str, x: float, y: float, size: float) -> None:
+#  def word(self, word: str, x: float, y: float, w: float, h: float) -> None:
 #  def container(self, word: str, x: float, y: float, w: float, h: float) -> None:
 #  def render(self):
 
-import render;
+import render
+from dataclasses import dataclass
+from copy import deepcopy
 
-pass
+def parse(text: str, render: Render):
+  @dataclass
+  class Block:
+    c: list[type["Block"]]
+    ic: bool
+    t: str
+    ct: str
+    ctp: bool
+    w: float
+    h: float
+    d: bool
+    i: int
+  @dataclass
+  class Pa:
+    c: type["Pa"]
+    i: int
+  pa=Pa(None,0)
+  text = ''.join(text.split())
+  m=dc([], True, '', text, False, 0, 0, False, 0)
+  while not m.d:
+    i=m
+    p=Pa
+    while len(i.c):
+      if not i.d:
+        i=i.c[i.d]
+        
+      else:
+        
+  #draw
+  render.render()
